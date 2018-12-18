264936a80565856c4a2d6db6287da1a543ad28a8
SandorEmanuel <sandor_emi@yahoo.com>
2018-12-18 22:53:19 +0200

    set-up advanced training project

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -1,13 +1,35 @@
 package org.fasttrackit;
 
-/**
- * Hello world!
- *
- */
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Car car = new Car ();
+        car.name = "Dacia";
+        car.color = "white";
+        car.doorCount = 5;
+        car.mileage = 7.5;
+        car.running = true;
+
+        Engine engine = new Engine();
+        engine.manufacturer = "Renault";
+        engine.capacity = 1500;
+
+        car.engine = engine;
+
+        car.engine.manufacturer = "BMW";
+
+        car.engine.horsepower = 200.5;
+        car.weight = 1200;
+
+        double x = 1.25;
+        // coeficient pentru calcului acceleratiei
+
+        car.acceleration = ((car.engine.horsepower / car.weight)* x);
+        // testare operatii matematice - nu reprezinta cu adevarat formula de calcul pentru acceleratie :)
+
+        System.out.println("Acceleratia masinii este:" + car.acceleration);
+
     }
 }
